3ebfe1277d02519bfc59aca16744780752f48538
Lintao <Lintao.Amons@outlook.com>
2022-04-05 22:16:38 +0800

    add key wrapper

--- src/core/Keyboard.tsx
+++ src/core/Keyboard.tsx
@@ -28,75 +28,82 @@ const Keyboard: React.FC = () => {
     return desc == undefined ? undefined : desc
   }
 
+  const KeyWrapper: React.FC<{ keycode: string, size?: number, hidden?: boolean }> = (props) => {
+    return (<OneButton keycode={props.keycode}
+      size={props.size}
+      hidden={props.hidden}
+      description={getCorrespondingDescription(props.keycode)} />)
+  }
+
   return (
     <div>
       <div className={styles.row1}>
-        <OneButton keycode='esc' />
-        <OneButton keycode='1' />
-        <OneButton keycode='2' />
-        <OneButton keycode='3' />
-        <OneButton keycode='4' />
-        <OneButton keycode='5' />
-        <OneButton keycode='6' />
-        <OneButton keycode='7' />
-        <OneButton keycode='8' />
-        <OneButton keycode='9' />
-        <OneButton keycode='0' />
-        <OneButton keycode='-' />
-        <OneButton keycode='=' />
-        <OneButton keycode='backspace' size={30} />
+        <KeyWrapper keycode='esc' />
+        <KeyWrapper keycode='1' />
+        <KeyWrapper keycode='2' />
+        <KeyWrapper keycode='3' />
+        <KeyWrapper keycode='4' />
+        <KeyWrapper keycode='5' />
+        <KeyWrapper keycode='6' />
+        <KeyWrapper keycode='7' />
+        <KeyWrapper keycode='8' />
+        <KeyWrapper keycode='9' />
+        <KeyWrapper keycode='0' />
+        <KeyWrapper keycode='-' />
+        <KeyWrapper keycode='=' />
+        <KeyWrapper keycode='backspace' size={30} />
       </div>
       <div className={styles.row2}>
-        <OneButton keycode='tab' size={20} />
-        <OneButton keycode='q' description={getCorrespondingDescription('q')} />
-        <OneButton keycode='w' />
-        <OneButton keycode='e' />
-        <OneButton keycode='r' />
-        <OneButton keycode='t' />
-        <OneButton keycode='y' />
-        <OneButton keycode='u' />
-        <OneButton keycode='i' />
-        <OneButton keycode='o' />
-        <OneButton keycode='p' />
-        <OneButton keycode='[' />
-        <OneButton keycode=']' />
-        <OneButton keycode='\' size={20} />
+        <KeyWrapper keycode='tab' size={20} />
+        <KeyWrapper keycode='q' />
+        <KeyWrapper keycode='w' />
+        <KeyWrapper keycode='e' />
+        <KeyWrapper keycode='r' />
+        <KeyWrapper keycode='t' />
+        <KeyWrapper keycode='y' />
+        <KeyWrapper keycode='u' />
+        <KeyWrapper keycode='i' />
+        <KeyWrapper keycode='o' />
+        <KeyWrapper keycode='p' />
+        <KeyWrapper keycode='[' />
+        <KeyWrapper keycode=']' />
+        <KeyWrapper keycode='\' size={20} />
       </div>
       <div className={styles.row3}>
-        <OneButton keycode='control' size={25} />
-        <OneButton keycode='a' />
-        <OneButton keycode='s' />
-        <OneButton keycode='d' />
-        <OneButton keycode='f' />
-        <OneButton keycode='g' />
-        <OneButton keycode='h' />
-        <OneButton keycode='j' />
-        <OneButton keycode='k' />
-        <OneButton keycode='l' />
-        <OneButton keycode=';' />
-        <OneButton keycode="'" />
-        <OneButton keycode='Enter' size={40} />
+        <KeyWrapper keycode='control' size={25} />
+        <KeyWrapper keycode='a' />
+        <KeyWrapper keycode='s' />
+        <KeyWrapper keycode='d' />
+        <KeyWrapper keycode='f' />
+        <KeyWrapper keycode='g' />
+        <KeyWrapper keycode='h' />
+        <KeyWrapper keycode='j' />
+        <KeyWrapper keycode='k' />
+        <KeyWrapper keycode='l' />
+        <KeyWrapper keycode=';' />
+        <KeyWrapper keycode="'" />
+        <KeyWrapper keycode='Enter' size={40} />
       </div>
       <div className={styles.row4}>
-        <OneButton keycode='shift' size={40} />
-        <OneButton keycode='z' />
-        <OneButton keycode='x' />
-        <OneButton keycode='c' />
-        <OneButton keycode='v' />
-        <OneButton keycode='b' />
-        <OneButton keycode='n' />
-        <OneButton keycode='m' />
-        <OneButton keycode='m' />
-        <OneButton keycode='.' />
-        <OneButton keycode='/' />
-        <OneButton keycode="shift" size={50} />
+        <KeyWrapper keycode='shift' size={40} />
+        <KeyWrapper keycode='z' />
+        <KeyWrapper keycode='x' />
+        <KeyWrapper keycode='c' />
+        <KeyWrapper keycode='v' />
+        <KeyWrapper keycode='b' />
+        <KeyWrapper keycode='n' />
+        <KeyWrapper keycode='m' />
+        <KeyWrapper keycode='m' />
+        <KeyWrapper keycode='.' />
+        <KeyWrapper keycode='/' />
+        <KeyWrapper keycode="shift" size={50} />
       </div>
       <div className={styles.row5}>
-        <OneButton keycode='Placeholder' hidden={true} />
-        <OneButton keycode='alt' />
-        <OneButton keycode='cmd' />
-        <OneButton keycode='space' size={100} />
-        <OneButton keycode='right cmd' />
+        <KeyWrapper keycode='Placeholder' hidden={true} />
+        <KeyWrapper keycode='alt' />
+        <KeyWrapper keycode='cmd' />
+        <KeyWrapper keycode='space' size={100} />
+        <KeyWrapper keycode='right cmd' />
       </div>
     </div>
   )
