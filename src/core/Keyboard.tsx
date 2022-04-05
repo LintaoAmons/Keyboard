688ea100a78acc1bdf145d23b8ef2fa582a7b9b8
Lintao <Lintao.Amons@outlook.com>
2022-04-05 18:44:18 +0800

    row 0 completed

--- src/core/Keyboard.tsx
+++ src/core/Keyboard.tsx
@@ -1,10 +1,26 @@
 import { OneButton } from "./OneButton/OneButton";
 import { Modifier } from "./OneButton/OneButton";
+import styles from './Keyboard.module.scss'
 
 const Keyboard: React.FC = () => {
   return (
     <div>
-      <OneButton keycode='j'/>
+      <div className={styles.row0}>
+        <OneButton keycode='esc'/>
+        <OneButton keycode='1'/>
+        <OneButton keycode='2'/>
+        <OneButton keycode='3'/>
+        <OneButton keycode='4'/>
+        <OneButton keycode='5'/>
+        <OneButton keycode='6'/>
+        <OneButton keycode='7'/>
+        <OneButton keycode='8'/>
+        <OneButton keycode='9'/>
+        <OneButton keycode='0'/>
+        <OneButton keycode='-'/>
+        <OneButton keycode='='/>
+        <OneButton keycode='backspace' size={3}/>
+      </div>
     </div>
   )
 }
--- src/core/OneButton/OneButton.tsx
+++ src/core/OneButton/OneButton.tsx
@@ -1,9 +1,11 @@
 import styles from './OneButton.module.scss';
 export interface OneButtonProps {
   keycode: String;
+  size?: number; 
   modifiers?: Modifier[]
   description?: String;
 }
+
 export enum Modifier {
   CMD,
   CTRL,
@@ -12,8 +14,18 @@ export enum Modifier {
 }
 
 const OneButton: React.FC<OneButtonProps> = (props) => {
+  const buttonSize : (size?: number) => string = (size) => {
+    switch (size) {
+      case 2: return styles.size2
+      case 3: return styles.size3
+      case 4: return styles.size3
+      case 5: return styles.size4
+      default: return styles.size1
+    }
+  }
+
   return (
-    <div className={styles.button}>
+    <div className={buttonSize(props.size)}>
       <span>{props.keycode}</span>
     </div>
   )
