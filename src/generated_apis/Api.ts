2b5855b1a53ee029a4b055bee170549d8573547a
Lintao <Lintao.Amons@outlook.com>
2022-05-29 17:12:56 +0800

    hard code link... Because I don't know how to set this by env

--- src/generated_apis/Api.ts
+++ src/generated_apis/Api.ts
@@ -87,7 +87,7 @@ export enum ContentType {
 }
 
 export class HttpClient<SecurityDataType = unknown> {
-    public baseUrl: string = 'http://localhost:80/api';
+    public baseUrl: string = 'https://oatnil.top/keyboard/api/';
     private securityData: SecurityDataType | null = null;
     private securityWorker?: ApiConfig<SecurityDataType>['securityWorker'];
     private abortControllers = new Map<CancelToken, AbortController>();
