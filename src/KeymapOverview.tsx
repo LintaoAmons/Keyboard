0113be1359a5cf51b80b9ec3277777366d551e92
Lintao <lintao.amons@outlook.com>
2023-11-05 10:35:35 +0800

    fine tune style

--- src/KeymapOverview.tsx
+++ src/KeymapOverview.tsx
@@ -37,7 +37,7 @@ export default function KeymapOverview(): JSX.Element {
                                             <td className="whitespace-nowrap px-6 py-4">{item.description}</td>
                                             <td className="whitespace-nowrap px-6 py-4">{item.achieveBy}</td>
                                             <td className="border-x-2 text-center">
-                                                <TERipple>
+                                                <TERipple className="mx-2">
                                                     <button
                                                         type="button"
                                                         className="inline-block rounded border-2 border-neutral-800 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-neutral-800 transition duration-150 ease-in-out hover:border-neutral-800 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-800 focus:border-neutral-800 focus:text-neutral-800 focus:outline-none focus:ring-0 active:border-neutral-900 active:text-neutral-900 dark:border-neutral-900 dark:text-neutral-900 dark:hover:border-neutral-900 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10 dark:hover:text-neutral-900 dark:focus:border-neutral-900 dark:focus:text-neutral-900 dark:active:border-neutral-900 dark:active:text-neutral-900"
