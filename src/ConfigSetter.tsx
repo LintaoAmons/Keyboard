e3b2769729a75557c24f197be973c644159acb62
Lintao <lintao.amons@outlook.com>
2023-11-05 15:50:56 +0800

    amend the textArea

--- src/ConfigSetter.tsx
+++ src/ConfigSetter.tsx
@@ -97,33 +97,21 @@ export default function ConfigSetter(): JSX.Element {
                     </button>
                 </TERipple>
             </div>
-            <div className="block max-w-sm rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-                <form>
-                    <div className="flex justify-center">
-                        <div className="relative mb-3 xl:w-96">
-                            <TETextarea 
-                                className="h-screen"
-                                id="Current Config" 
-                                label="Message"
-                                onChange={(e: any) => setTextAreaContent(e.target.value)}
-                                onBlur={handleConfigInputAreaBlur}
-                                value={textAreaContent}
 
-                            ></TETextarea>
-                        </div>
-                    </div>
+            <div className="flex justify-center mt-5">
+                <div className="relative mb-3 xl:w-96">
+                    <TETextarea
+                        className="h-screen"
+                        id="Current Config"
+                        label="Message"
+                        onChange={(e: any) => setTextAreaContent(e.target.value)}
+                        onBlur={handleConfigInputAreaBlur}
+                        value={textAreaContent}
 
-                    {/* <!--Submit button--> */}
-                    <TERipple rippleColor="light" className="w-full">
-                        <button
-                            type="button"
-                            className="block w-full rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]]"
-                        >
-                            Subscribe
-                        </button>
-                    </TERipple>
-                </form>
+                    ></TETextarea>
+                </div>
             </div>
+
 
         </div>
     )
