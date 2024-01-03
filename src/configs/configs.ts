8dfedf859c35a4853b4bf755c3b124024f6aed49
Lintao <lintao.amons@outlook.com>
2024-01-03 12:20:24 +0800

    remove the pr example

--- src/configs/configs.ts
+++ src/configs/configs.ts
@@ -1,10 +1,8 @@
 import { KeyboardConfigJson } from '../Config';
 import Default from './default.json'
-import PrExample from './submit-pr-to-add-yours.json'
 
 const configs: KeyboardConfigJson[] = [
     Default,
-    PrExample
 ]
 
 export default configs;
